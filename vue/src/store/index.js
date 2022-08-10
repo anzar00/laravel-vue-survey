89d9a34b0a270765fb4bfde336fd98668f6a7787
anzar00 <peerzada.anzar@gmail.com>
2022-08-11 01:51:42 +0530

    vue init

---
+++ vue/src/store/index.js
@@ -0,0 +1,16 @@
+import {createStore} from 'vuex';
+
+const store = createStore({
+    state: {
+        user:{
+            data: {name: 'Anzar'},
+            token: null
+        }
+    },
+    getters: {},
+    mutations: {},
+    actions: {},
+    modules: {}
+})
+
+export default store;
